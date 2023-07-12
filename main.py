290e3a0d166e30661d57046bd37ea5088a9f376c
Felipe Quiñonez <fdquinones@hotmail.com>
2023-07-12 21:42:40 +0000

    ajustes a la plantilla

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 # Lista de personas
-personas = [{"nombre": "Juan", "apellido": "Perez", "edad": 25},
+personaList = [{"nombre": "Juan", "apellido": "Perez", "edad": 25},
             {"nombre": "Ana", "apellido": "Gomez", "edad": 30},
             {"nombre": "Carlos", "apellido": "Lopez", "edad": 45}]
 
 @app.route('/')
 def home():
-    return render_template('index.html', personas=personas)
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/personas')
+def personas():
+    return render_template('personas.html', personas=personaList)
+
+@app.route('/personas', methods=['POST'])
+def add():
+    print("llego por aqui a guardar")
+    nombre = request.form.get('nombre')
+    apellido = request.form.get('apellido')
+    edad = int(request.form.get('edad'))
+
+    personaList.append({"nombre": nombre, "apellido": apellido, "edad": edad})
+
+    return redirect(url_for('personas'))
 
 if __name__ == '__main__':
     app.run(debug=True)
